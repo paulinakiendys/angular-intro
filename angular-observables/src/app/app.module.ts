0c94fbe72edda97caafc98762af352145bb7da2c
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-07-06 15:01:37 +0200

    Created a component

--- angular-observables/src/app/app.module.ts
+++ angular-observables/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicComponent } from './components/basic/basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent
   ],
   imports: [
     BrowserModule
