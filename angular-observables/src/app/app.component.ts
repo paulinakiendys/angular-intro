3fd145d23629155371afca41aa36f844783e47a5
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-07-06 15:30:20 +0200

    Logging numbers with interval

--- angular-observables/src/app/app.component.ts
+++ angular-observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +9,44 @@ import { of } from 'rxjs';
 export class AppComponent {
   title = 'angular-observables';
 
-  source = of(1, 2, 3);
-
-  observer = {
-    next: (x: number) => {
-      console.log(x);
-    },
-    error: (err: any) => {
-      console.log(err);
-    },
-    complete: () => {
-      console.log('Completed')
-    }
-  }
-
   ngOnInit(): void {
-    this.source.subscribe(this.observer);
+   let slowObservable = new Observable<number>((o) => {
+      let sequence: number[] = [1, 2, 3];
+      let timeout: NodeJS.Timeout;
+
+      function getValue(i: number) {
+        timeout = setTimeout(() => {
+          o.next(sequence[i]);
+
+          if (i === sequence.length - 1) {
+            o.complete();
+          } else {
+            getValue(++i);
+          }
+        }, 1000)
+      }
+
+      getValue(0);
+
+      return {
+        unsubscribe() {
+          clearTimeout(timeout);
+        }
+      }
+    })
+
+    let observer = {
+      next: (x: number) => {
+        console.log(x);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('Completed')
+      }
+    }
+
+    slowObservable.subscribe(observer);
   }
 }
