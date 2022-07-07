d08faf1988b337d3d2fe5d71ae36ec34c6c5a0ef
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-07-07 13:31:48 +0200

    Created zoo component

--- angular-services/src/app/app.module.ts
+++ angular-services/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ZooComponent } from './components/zoo/zoo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZooComponent
   ],
   imports: [
     BrowserModule
