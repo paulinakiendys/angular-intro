ebe4f3a3be29268bf555a2ced0bfde38cafd0e8a
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-07-07 13:27:48 +0200

    Added animal class

---
+++ angular-services/src/app/models/Animal.ts
@@ -0,0 +1,11 @@
+export class Animal {
+    name: string;
+    age: number;
+    isHungry: boolean;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+        this.isHungry = false;
+    }
+}
