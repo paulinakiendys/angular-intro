c58c579f7a4c3f56ddac3b9e06ec39d6d5547ca2
Paulina Kiendys <84099095+paulinakiendys@users.noreply.github.com>
2022-07-13 15:31:59 +0200

    Created components

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
